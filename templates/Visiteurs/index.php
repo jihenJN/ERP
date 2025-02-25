59c271fcb08464048a60830a46fd1319e0ac99b3
jihenJN <jihenjihen23@gmail.com>
2025-02-25 12:08:42 +0100

    index visiteur en cour

--- templates/Visiteurs/index.php
+++ templates/Visiteurs/index.php
@@ -1,46 +1,167 @@
+
+<!-- Content Header (Page header) -->
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.0/jquery.min.js" type="text/javascript"></script>
 <?php
-/**
- * @var \App\View\AppView $this
- * @var iterable<\App\Model\Entity\Visiteur> $visiteurs
- */
+echo $this->Html->script('salma');
 ?>
-<div class="visiteurs index content">
-    <?= $this->Html->link(__('New Visiteur'), ['action' => 'add'], ['class' => 'button float-right']) ?>
-    <h3><?= __('Visiteurs') ?></h3>
-    <div class="table-responsive">
-        <table>
+<?php
+//$add = "";
+//$edit = "";
+//$delete = "";
+
+$add = 1;
+$edit = 1;
+$delete = 1;
+$view = "";
+$session = $this->request->getSession();
+$abrv = $session->read('abrvv');
+$lien = $session->read('lien_parametrage' . $abrv);
+//debug($lien);die;
+foreach ($lien as $k => $liens) {
+  if (@$liens['lien'] == 'visites') {
+    $add = $liens['ajout'];
+    $edit = $liens['modif'];
+    $delete = $liens['supp'];
+  }
+  //debug($liens);die;
+}
+
+if ($add == 1) { ?>
+<div class="pull-left" style="margin-left:25px;margin-top: 20px">
+  <?php echo $this->Html->link(__('Ajouter'), ['action' => 'Add'], ['class' => 'btn btn-success btn-sm']) ?>
+</div>
+<?php } ?>
+<br><br><br>
+<section class="content-header">
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.0/jquery.min.js" type="text/javascript"></script>
+</section>
+<h1>
+Visiteurs
+</h1>
+
+<section class="content">
+  <div class="row">
+    <div class="col-xs-12">
+      <div class="box">
+        <div class="box-body">
+          <table id="example1" class="table table-bordered table-striped">
             <thead>
-                <tr>
-                    <th><?= $this->Paginator->sort('id') ?></th>
-                    <th><?= $this->Paginator->sort('nom') ?></th>
-                    <th><?= $this->Paginator->sort('telephone') ?></th>
-                    <th class="actions"><?= __('Actions') ?></th>
-                </tr>
+              <tr>
+                <th width="40%" align="center"><?= ('Nom') ?></th>
+                <th width="30%" align="center"><?= ('Téléphone') ?></th>
+                <th width="50%" scope="col" class="actions text-center"><?= __('Actions') ?></th>
+              </tr>
             </thead>
             <tbody>
-                <?php foreach ($visiteurs as $visiteur): ?>
+
+            <?php foreach ($visiteurs as $i => $visiteur): ?>
                 <tr>
-                    <td><?= $this->Number->format($visiteur->id) ?></td>
-                    <td><?= h($visiteur->nom) ?></td>
-                    <td><?= h($visiteur->telephone) ?></td>
-                    <td class="actions">
-                        <?= $this->Html->link(__('View'), ['action' => 'view', $visiteur->id]) ?>
-                        <?= $this->Html->link(__('Edit'), ['action' => 'edit', $visiteur->id]) ?>
-                        <?= $this->Form->postLink(__('Delete'), ['action' => 'delete', $visiteur->id], ['confirm' => __('Are you sure you want to delete # {0}?', $visiteur->id)]) ?>
-                    </td>
+                <td><?= h($visiteur->nom) ?>
+                    <?php echo $this->Form->control('id', ['index' => $i, 'id' => 'id'.$i, 'value' => $visiteur->id, 'label' => '', 'type'=>'hidden','champ' => 'id', 'class' => 'form-control']); ?>
+
+                </td>
+                <td><?= h($visiteur->telephone) ?></td>
+                  <td class="actions text" align="center">
+                    <?php echo $this->Html->link("<button class='btn btn-xs btn-success'><i class='fa fa-search'></i></button>", array('action' => 'view', $visiteur->id), array('escape' => false)); ?>
+                    <?php if ($edit == 1) { echo $this->Html->link("<button class='btn btn-xs btn-warning'><i class='fa fa-edit'></i></button>", array('action' => 'edit', $visiteur->id), array('escape' => false)); }?>
+                    <?php if ($delete == 1) {//echo $this->Form->postLink("<button class='btn btn-xs btn-danger'><i class='fa fa-trash-o'></i></button>", array('action' => 'delete', $visiteur->id), array('escape' => false, null), __('Veuillez vraiment supprimer cette enregistrement # {0}?', $visiteur->id)); ?>
+                    <button index='<?php echo $i?>' class='verifiervisite btn btn-xs btn-danger'><i class='fa fa-trash-o'></i></button>
+                    <?php } ?>
+                  </td>
+                  
                 </tr>
-                <?php endforeach; ?>
+              <?php endforeach; ?>
             </tbody>
-        </table>
+          </table>
+        </div>
+        <!-- /.box-body -->
+      </div>
+      <!-- /.box -->
     </div>
-    <div class="paginator">
-        <ul class="pagination">
-            <?= $this->Paginator->first('<< ' . __('first')) ?>
-            <?= $this->Paginator->prev('< ' . __('previous')) ?>
-            <?= $this->Paginator->numbers() ?>
-            <?= $this->Paginator->next(__('next') . ' >') ?>
-            <?= $this->Paginator->last(__('last') . ' >>') ?>
-        </ul>
-        <p><?= $this->Paginator->counter(__('Page {{page}} of {{pages}}, showing {{current}} record(s) out of {{count}} total')) ?></p>
-    </div>
-</div>
+  </div>
+</section>
+<?php echo $this->Html->css('AdminLTE./bower_components/datatables.net-bs/css/dataTables.bootstrap.min', ['block' => 'css']); ?>
+
+<!-- DataTables -->
+<?php echo $this->Html->script('AdminLTE./bower_components/datatables.net/js/jquery.dataTables.min', ['block' => 'script']); ?>
+<?php echo $this->Html->script('AdminLTE./bower_components/datatables.net-bs/js/dataTables.bootstrap.min', ['block' => 'script']); ?>
+
+<?php $this->start('scriptBottom'); ?>
+<script>
+  $(function() {
+    $('#example1').DataTable()
+    $('#example2').DataTable({
+      'paging': true,
+      'lengthChange': false,
+      'searching': false,
+      'ordering': true,
+      'info': true,
+      'autoWidth': false
+    })
+  })
+</script>
+<?php $this->end(); ?>
+
+
+<script type="text/javascript">
+
+$(function() {
+    $('.verifiervisite').on('click', function() {
+      let ind = $(this).attr('index');
+      let id = $('#id' + ind).val();
+
+      $.ajax({
+        method: "GET",
+        url: "<?= $this->Url->build(['controller' => 'Visites', 'action' => 'verifvisitesup']) ?>",
+        dataType: "json",
+        data: {
+          id: id
+        },
+        headers: {
+          'X-CSRF-Token': $('meta[name="csrfToken"]').attr('content')
+        },
+        success: function(data, status, settings) {
+          if (data.Comptes != 0) {
+            alert('existe dans un document');
+          } else {
+            if (confirm('Voulez-vous supprimer cet enregistrement')) {
+              document.location = wr+"Visites/delete/" + id;
+            }
+          }
+        }
+      });
+    });
+  });
+</script>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
