6951a9faae3e08850fd3ee000485c4b8ba5ba1f5
jihenJN <jihenjihen23@gmail.com>
2025-03-24 10:16:21 +0100

    chores

--- templates/Demandeoffredeprixes/view.php
+++ templates/Demandeoffredeprixes/view.php
@@ -55,7 +55,7 @@
 
 
 
-             
+
 
 
 
@@ -67,166 +67,117 @@
 
                 <section class="content" style="width: 99%">
 
-<div class="tab-content" id="fichart">
+                    <div class="tab-content" id="fichart">
 
-    <div class="panel panel-default">
-        <div class="panel-heading">
-            <h3 class="panel-title"><?= __('Fournisseurs') ?></h3>
-            <a class="btn btn-primary ajouterlignematriceee" table="addtablea" index="index" tr="tra"
-               style="float: right; position: relative; top: -25px; pointer-events: none; opacity: 0.5;">
-                <i class="fa fa-plus-circle"></i>
-            </a>
-        </div>
-        <div class="panel-body">
-            <table class="table table-bordered table-striped table-bottomless" id="addtablea"
-                   style="width:100%" align="center">
-                <thead>
-                    <tr bgcolor="#EDEDED">
-                        <td align="center">Fournisseur</td>
-                        <td align="center"></td>
-                    </tr>
-                </thead>
-                <tbody>
-                    <!-- Loop through existing 'lignedemandeoffredeprixes' -->
-                    <?php foreach ($demandeoffredeprix->lignedemandeoffredeprixes as $index => $ligne) : ?>
-                        <tr champ="tra" class="tra">
-                            <td align="left">
-                                <?= $this->Form->hidden("lignedemandeoffredeprixes.$index.id", ['value' => $ligne->id]) ?>
-                                <div style="margin-top:10px">
-                                    <?= $this->Form->control("lignedemandeoffredeprixes.$index.fournisseur_id", [
-                                        'options' => $fournisseurs,
-                                        'label' => false,
-                                        'empty' => 'Veuillez choisir',
-                                        'class' => 'form-control',
-                                        'value' => $ligne->fournisseur_id
-                                    ]) ?>
-                                </div>
-                            </td>
-                          
-                        </tr>
-
-                        <tr class="traa" champ='traa'>
-                            <td width='30%'></td>
-                            <td champ="afef" class="afef" colspan="3">
-                                <div class="panel panel-default">
-                                    <div class="panel-heading" >
-                                        <h3 class="panel-title"><?= __('Article') ?></h3>
-                                        <a class="btn btn-primary ajouterligne1" tabletype='addtableaa'
-                                           indexlignetype='indexa' trtype="traaa"
-                                           style="float: right; position: relative; top: -25px; pointer-events: none; opacity: 0.5;">
-                                            <i class="fa fa-plus-circle"></i>
-                                        </a>
-                                    </div>
-                                    <div class="panel-body">
-                                        <table class="table table-bordered table-striped table-bottomless"
-                                               index="" indexligne='indexa' champ="addtableaa"
-                                               style="width:100%" align="center">
-                                            <thead>
-                                                <tr bgcolor="#EDEDED">
-                                                    <td align="center">Article</td>
-                                                    <td align="center">Quantité</td>
-                                                    <td align="center"></td>
-                                                </tr>
-                                            </thead>
-                                            <tbody>
-                                                <tr class="traaa" champ='traaa'>
-                                                    <td>
-                                                        <?= $this->Form->control("lignedemandeoffredeprixes.$index.article_id", [
-                                                            'options' => $articles,
-                                                            'label' => false,
-                                                            'empty' => 'Veuillez Choisir',
-                                                            'class' => 'form-control',
-                                                            'value' => $ligne->article_id
-                                                        ]) ?>
-                                                    </td>
-                                                    <td>
-                                                        <?= $this->Form->control("lignedemandeoffredeprixes.$index.qte", [
-                                                            'label' => false,
-                                                            'class' => 'form-control',
-                                                            'value' => $ligne->qte
-                                                        ]) ?>
-                                                    </td>
-                                                   
-                                                </tr>
-                                            </tbody>
-                                        </table>
-                                        <input type="hidden" value="-1" class="" champ="indexa" />
-                                    </div>
-                                </div>
-                            </td>
-                        </tr>
-                    <?php endforeach; ?>
-
-                    <!-- Template row for adding new fournisseurs (Hidden for JS cloning) -->
-                    <tr class="tra" champ="tra" style="display:none;">
-                        <td align="left">
-                            <?= $this->Form->hidden('lignedemandeoffredeprixes.__INDEX__.id', ['value' => '']) ?>
-                            <div style="margin-top:10px">
-                                <?= $this->Form->control('lignedemandeoffredeprixes.__INDEX__.fournisseur_id', [
-                                    'options' => $fournisseurs,
-                                    'label' => false,
-                                    'empty' => 'Veuillez choisir',
-                                    'class' => 'form-control'
-                                ]) ?>
+                        <div class="panel panel-default">
+                            <div class="panel-heading">
+                                <h3 class="panel-title"><?= __('Fournisseurs') ?></h3>
+                                <a class="btn btn-primary ajouterlignematriceee" table="addtablea" index="index" tr="tra"
+                                    style="float: right; position: relative; top: -25px; pointer-events: none; opacity: 0.5;">
+                                    <i class="fa fa-plus-circle"></i>
+                                </a>
                             </div>
-                        </td>
-                       
-                    </tr>
+                            <div class="panel-body">
+                                <table class="table table-bordered table-striped table-bottomless" id="addtablea"
+                                    style="width:100%" align="center">
+                                    <thead>
+                                        <tr bgcolor="#EDEDED">
+                                            <td align="center">Fournisseur</td>
+                                            <td align="center"></td>
+                                        </tr>
+                                    </thead>
+                                    <tbody>
+                                        <!-- Loop through existing 'lignedemandeoffredeprixes' -->
+                                        <?php foreach ($demandeoffredeprix->lignedemandeoffredeprixes as $index => $ligne) : ?>
+                                            <tr champ="tra" class="tra">
+                                                <td align="left">
+                                                    <?= $this->Form->hidden("lignedemandeoffredeprixes.$index.id", ['value' => $ligne->id]) ?>
+                                                    <div style="margin-top:10px">
+                                                        <?= $this->Form->control("lignedemandeoffredeprixes.$index.fournisseur_id", [
+                                                            'options' => $fournisseurs,
+                                                            'label' => false,
+                                                            'empty' => 'Veuillez choisir',
+                                                            'class' => 'form-control',
+                                                            'value' => $ligne->fournisseur_id
+                                                        ]) ?>
+                                                    </div>
+                                                </td>
 
-                </tbody>
-            </table>
-            <input type="hidden" value="-1" id="index" />
-        </div>
-    </div>
-</div>
+                                            </tr>
 
-</section>
+                                            <tr class="traa" champ='traa'>
+                                                <td width='30%'></td>
+                                                <td champ="afef" class="afef" colspan="3">
+                                                    <div class="panel panel-default">
+                                                        <div class="panel-heading">
+                                                            <h3 class="panel-title"><?= __('Article') ?></h3>
+                                                            <a class="btn btn-primary ajouterligne1" tabletype='addtableaa'
+                                                                indexlignetype='indexa' trtype="traaa"
+                                                                style="float: right; position: relative; top: -25px; pointer-events: none; opacity: 0.5;">
+                                                                <i class="fa fa-plus-circle"></i>
+                                                            </a>
+                                                        </div>
+                                                        <div class="panel-body">
+                                                            <table class="table table-bordered table-striped table-bottomless"
+                                                                index="" indexligne='indexa' champ="addtableaa"
+                                                                style="width:100%" align="center">
+                                                                <thead>
+                                                                    <tr bgcolor="#EDEDED">
+                                                                        <td align="center">Article</td>
+                                                                        <td align="center">Quantité</td>
+                                                                        <td align="center"></td>
+                                                                    </tr>
+                                                                </thead>
+                                                                <tbody>
+                                                                    <tr class="traaa" champ='traaa'>
+                                                                        <td>
+                                                                            <?= $this->Form->control("lignedemandeoffredeprixes.$index.article_id", [
+                                                                                'options' => $articles,
+                                                                                'label' => false,
+                                                                                'empty' => 'Veuillez Choisir',
+                                                                                'class' => 'form-control',
+                                                                                'value' => $ligne->article_id
+                                                                            ]) ?>
+                                                                        </td>
+                                                                        <td>
+                                                                            <?= $this->Form->control("lignedemandeoffredeprixes.$index.qte", [
+                                                                                'label' => false,
+                                                                                'class' => 'form-control',
+                                                                                'value' => $ligne->qte
+                                                                            ]) ?>
+                                                                        </td>
 
+                                                                    </tr>
+                                                                </tbody>
+                                                            </table>
+                                                            <input type="hidden" value="-1" class="" champ="indexa" />
+                                                        </div>
+                                                    </div>
+                                                </td>
+                                            </tr>
+                                        <?php endforeach; ?>
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+                                        <!-- Template row for adding new fournisseurs (Hidden for JS cloning) -->
+                                        <tr class="tra" champ="tra" style="display:none;">
+                                            <td align="left">
+                                                <?= $this->Form->hidden('lignedemandeoffredeprixes.__INDEX__.id', ['value' => '']) ?>
+                                                <div style="margin-top:10px">
+                                                    <?= $this->Form->control('lignedemandeoffredeprixes.__INDEX__.fournisseur_id', [
+                                                        'options' => $fournisseurs,
+                                                        'label' => false,
+                                                        'empty' => 'Veuillez choisir',
+                                                        'class' => 'form-control'
+                                                    ]) ?>
+                                                </div>
+                                            </td>
+                                        </tr>
+                                    </tbody>
+                                </table>
+                                <input type="hidden" value="-1" id="index" />
+                            </div>
+                        </div>
+                    </div>
+                </section>
                 <?php echo $this->Form->end(); ?>
             </div>
             <!-- /.box -->
